8fa878051be0de97160b29d9439778118a40bcf1
Ilin Oleg <ilinoa90@gmail.com>
2021-06-06 23:08:22 +0300

    Update Model.js

--- app/src/Model/Model.js
+++ app/src/Model/Model.js
@@ -5,12 +5,10 @@ const Model = (props) => {
     const fbx = useFBX(props.url);
     const mesh = useRef()
     const [active, setActive] = useState(false)
-    console.log(mesh.color)
     return (
         <mesh
             {...props}
             ref={mesh}
-            color={"red"}
             rotation={[Math.PI / -2, 0, 0]}
             position={active ? [0, 0, 0] : [0, 1, 0]}
             onClick={(event) => setActive(!active)}
