19107bedbe9dd78c58c533dfed08e630d9ce9f74
root <root@ip-172-26-7-218.eu-west-2.compute.internal>
2021-06-30 20:00:49 +0000

    add server

---
+++ server/index.js
@@ -0,0 +1,27 @@
+
+const express = require("express");
+const app = express();
+const path = require('path');
+
+
+const PORT = 80;
+
+
+if(true) {
+    app.use('/', express.static(path.join(__dirname, '../app', 'public')));
+    app.get('*', function (req, res) {
+        res.sendFile(path.resolve(__dirname, '../app', 'public', 'index.html'))
+    })
+}
+
+
+async function start() {
+    try{
+	app.listen(PORT, () => console.log(`Starting the development server... ${PORT}`)); 
+    } catch(e) {
+        console.log("Server error", e.message)
+        process.exit(1)
+    }
+}
+
+start();
